df30ed932f5ba3d1a8d2fa1467d422141e420d4e
Nicolilo11 <nicoramitos@gmail.com>
2021-07-20 23:23:54 +0200

    <mobile.ts> Creacion de la clase Mobile

---
+++ mobile.ts
@@ -0,0 +1,24 @@
+class Mobile
+{
+    public name:string;
+    public model:string;
+    public tradeMark:string;
+    public sDSize:string;
+    public color:string;
+    public is5G:boolean;
+    public cameraNumber:number;
+    public price:number;
+
+    constructor(name:string, model:string, tradeMark:string,
+         sdSize:string, color:string, is5G:boolean, camaraNumber:number, price:number)
+    {
+        this.name = name;
+        this.model = model;
+        this.tradeMark = tradeMark;
+        this.sDSize = sdSize;
+        this.color = color;
+        this.is5G = is5G;
+        this.cameraNumber = camaraNumber;
+        this.price = price;
+    }
+}
